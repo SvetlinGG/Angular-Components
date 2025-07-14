759621d567b91618ea1bbdb9e6b84598be9079dc
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-15 00:42:04 +0200

    styling component

--- component-demo/src/app/navigation-bar/navigation-bar.component.ts
+++ component-demo/src/app/navigation-bar/navigation-bar.component.ts
@@ -2,9 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-navigation-bar',
-    template: `<div>Hello this is my navigation</div>`,
+    template: `<div class="nav-bar">Hello this is my navigation</div>`,
     standalone: true,
-    
+    styles: `.nav-bar {
+        width: 100%;
+        height: 30px;
+        padding: 10px;
+        background-color: skyblue;
+        color: black;
+    }`
 })
 
 export class NavigationBarComponent{}
