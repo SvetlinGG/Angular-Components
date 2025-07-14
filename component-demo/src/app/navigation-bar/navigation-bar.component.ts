fc4e898a5a938a08b1204c819ddf6416ca2da752
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-15 00:48:48 +0200

    more styling

--- component-demo/src/app/navigation-bar/navigation-bar.component.ts
+++ component-demo/src/app/navigation-bar/navigation-bar.component.ts
@@ -2,7 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-navigation-bar',
-    template: `<div class="nav-bar">Hello this is my navigation</div>`,
+    template: `<div class="nav-bar">
+        <div class="nav-btn">Home</div>
+        <div class="nav-btn">Contacts</div>
+        <div class="nav-btn">About</div>
+    </div>`,
     standalone: true,
     styles: `.nav-bar {
         width: 100%;
@@ -10,6 +14,15 @@ import { Component } from "@angular/core";
         padding: 10px;
         background-color: skyblue;
         color: black;
+        display: flex;
+
+        .nav-btn {
+            border: 1px solid red;
+            padding: 10px;
+        }
+        .nav-btn:hover {
+            cursor: pointer;
+        }
     }`
 })
 
