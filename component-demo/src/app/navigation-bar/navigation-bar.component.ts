6b2cc5337c90e99574007c1617ba33cccc0ccca4
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-15 00:34:45 +0200

    add component

---
+++ component-demo/src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-navigation-bar',
+    template: `<div>Hello this is my navigation</div>`,
+    standalone: true,
+    
+})
+
+export class NavigationBarComponent{}
