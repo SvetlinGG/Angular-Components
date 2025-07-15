faf15b72a0713654358dc1d0cc37f229f4b21d21
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-15 23:39:20 +0200

    component structure

--- component-demo/src/app/navigation-bar/navigation-bar.component.ts
+++ component-demo/src/app/navigation-bar/navigation-bar.component.ts
@@ -2,34 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-navigation-bar',
-    template: `<div class="nav-bar">
-        <div>{{title}}</div>
-        <div class="nav-btn">Home</div>
-        <div class="nav-btn">Contacts</div>
-        <div class="nav-btn">About</div>
-    </div>`,
+    templateUrl: './navigation-bar.component.html',
     standalone: true,
-    styles: `.nav-bar {
-        width: 100%;
-        height: 30px;
-        padding: 10px;
-        background-color: skyblue;
-        color: black;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        .nav-btn {
-            margin-left: 10px;
-            padding: 10px;
-            border: 1px solid black;
-        }
-        .nav-btn:hover {
-            cursor: pointer;
-            background-color: grey;
-            color: white;
-        }
-    }`
+    styleUrl: './navigation-bar.component.css'
 })
 
 export class NavigationBarComponent{
