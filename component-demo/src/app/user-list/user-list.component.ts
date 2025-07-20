657fad323899330c3b242c48c8491f63a9600f0d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-20 23:06:13 +0200

    connect components

--- component-demo/src/app/user-list/user-list.component.ts
+++ component-demo/src/app/user-list/user-list.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
