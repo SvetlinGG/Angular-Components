8a2e2ce08a2a7561936b895ced43963e0df8a629
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-21 23:04:07 +0100

    create user list

--- component-train/src/app/main/main.component.ts
+++ component-train/src/app/main/main.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  users = [
+    {id: 1, name: "Svetlin Garabedyan", age: 53},
+    {id: 2, name: "Ivan Ivanov", age: 33},
+    {id: 3, name: "Pesho Petrov", age: 43},
+    {id: 4, name: "Maria Marieva", age: 23},
+  ]
 }
