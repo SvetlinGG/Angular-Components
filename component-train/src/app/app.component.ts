968a9832507ebd6a22a5571f59c862faca5e21ce
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-22 15:49:26 +0100

    generate test demo component

--- component-train/src/app/app.component.ts
+++ component-train/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { TestDemoComponent } from './test-demo/test-demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, MainComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, MainComponent,TestDemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
